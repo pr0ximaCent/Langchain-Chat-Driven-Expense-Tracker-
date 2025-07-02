ca5f66feb9ec5188bc290f083bf14026d7f4abde
proximaCent <sabikaftahee01@gmail.com>
2025-07-02 22:48:33 +0600

    cleaned backend

--- backend/mongo_test.py
+++ backend/mongo_test.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 import certifi
 
-load_dotenv()  # This loads your .env into os.environ
-
+load_dotenv()
 MONGO_URI = os.environ["MONGO_URI"]
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 client.admin.command('ping')
